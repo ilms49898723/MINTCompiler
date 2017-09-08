adcd8ab3ac5b30ec83af150658e95292be3f3c01
LittleBird <ilms49898723@gmail.com>
2017-09-08 19:47:16 +0900

    fix mixer drawing

--- src/main/java/com/github/ilms49898723/fluigi/device/component/Mixer.java
+++ src/main/java/com/github/ilms49898723/fluigi/device/component/Mixer.java
@@ -42,7 +42,6 @@ public class Mixer extends BaseComponent {
             int y = (int) start.getY();
             int w = (int) end.getX();
             int h = (int) end.getY();
-            System.out.println(x + " " + y + " " + w + " " + h);
             g.setColor(Color.BLUE);
             g.fillRect(x, y, w, h);
         }
@@ -52,28 +51,27 @@ public class Mixer extends BaseComponent {
         Point2D startPoint = new Point2D.Double(0.0, 0.0);
         for (int i = 0; i < mNumBends; ++i) {
             mPoints.addAll(getSingleBend(startPoint));
-            startPoint = new Point2D.Double(startPoint.getX() + mChannelWidth + mBendSpacing, startPoint.getY());
-            System.out.println(startPoint);
+            startPoint = new Point2D.Double(startPoint.getX() + 2 * (mChannelWidth + mBendSpacing), startPoint.getY());
         }
     }
 
     private List<Point2DPair> getSingleBend(Point2D startPoint) {
         List<Point2DPair> result = new ArrayList<>();
-        Point2D pa1 = startPoint;
-        Point2D pa2 = new Point2D.Double(pa1.getX() + mChannelWidth, pa1.getY() + mChannelWidth + (mBendLength - mChannelWidth) / 2);
+        Point2D pa1 = new Point2D.Double(startPoint.getX(), startPoint.getY() - mChannelWidth - (mBendLength - mChannelWidth) / 2);
+        Point2D pa2 = new Point2D.Double(pa1.getX() + mChannelWidth, startPoint.getY());
         result.add(new Point2DPair(pa1, pa2));
-        Point2D pb1 = new Point2D.Double(pa2.getX() - mChannelWidth, pa2.getY() - mChannelWidth);
-        Point2D pb2 = new Point2D.Double(pa2.getX() + 2 * mChannelWidth + mBendSpacing, pa2.getY() + mChannelWidth);
+        Point2D pb1 = new Point2D.Double(pa1.getX(), pa1.getY());
+        Point2D pb2 = new Point2D.Double(pb1.getX() + 2 * mChannelWidth + mBendSpacing, pb1.getY() + mChannelWidth);
         result.add(new Point2DPair(pb1, pb2));
-//        Point2D pc1 = new Point2D.Double(pb2.getX() - mChannelWidth, pb2.getY() + mChannelWidth);
-//        Point2D pc2 = new Point2D.Double(pc1.getX() + mChannelWidth, pc1.getY() - mBendLength);
-//        result.add(new Point2DPair(pc1, pc2));
-//        Point2D pd1 = new Point2D.Double(pc2.getX() - mChannelWidth, pc2.getY() + mChannelWidth);
-//        Point2D pd2 = new Point2D.Double(pd1.getX() + 2 * mChannelWidth + mBendSpacing, pd1.getY() - mChannelWidth);
-//        result.add(new Point2DPair(pd1, pd2));
-//        Point2D pe1 = new Point2D.Double(pd2.getX() - mChannelWidth, pd2.getY() - (mBendLength - mChannelWidth) / 2 + mChannelWidth);
-//        Point2D pe2 = pd2;
-//        result.add(new Point2DPair(pe1, pe2));
+        Point2D pc1 = new Point2D.Double(pb2.getX() - mChannelWidth, pb2.getY() - mChannelWidth);
+        Point2D pc2 = new Point2D.Double(pc1.getX() + mChannelWidth, pc1.getY() + mBendLength);
+        result.add(new Point2DPair(pc1, pc2));
+        Point2D pd1 = new Point2D.Double(pc2.getX() - mChannelWidth, pc2.getY() - mChannelWidth);
+        Point2D pd2 = new Point2D.Double(pd1.getX() + 2 * mChannelWidth + mBendSpacing, pd1.getY() + mChannelWidth);
+        result.add(new Point2DPair(pd1, pd2));
+        Point2D pe1 = new Point2D.Double(pd2.getX() - mChannelWidth, pd2.getY() - (mBendLength - mChannelWidth) / 2 - mChannelWidth);
+        Point2D pe2 = new Point2D.Double(pd2.getX(), pd2.getY());
+        result.add(new Point2DPair(pe1, pe2));
         return result;
     }
 }
