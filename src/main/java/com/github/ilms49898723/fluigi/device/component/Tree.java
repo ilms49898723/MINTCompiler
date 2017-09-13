2d1b73a74553b4befb147ae1f2aa01989b7927eb
edinhon <m23127654@gmail.com>
2017-09-13 22:13:54 +0800

    Tree: complete setting port, not yet rotate

--- src/main/java/com/github/ilms49898723/fluigi/device/component/Tree.java
+++ src/main/java/com/github/ilms49898723/fluigi/device/component/Tree.java
@@ -2,9 +2,12 @@ package com.github.ilms49898723.fluigi.device.component;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.github.ilms49898723.fluigi.device.component.point.Point2DPair;
+import com.github.ilms49898723.fluigi.device.component.point.Point2DUtil;
 import com.github.ilms49898723.fluigi.device.symbol.ComponentLayer;
 import com.github.ilms49898723.fluigi.device.symbol.ComponentType;
 
@@ -68,15 +71,23 @@ public class Tree extends BaseComponent {
 			}
 			startPt = new Point2D(mPoints.get(0).getPointA().getX() + disNextLvl, startPt.getY() + (mChannelLength / 2) - mChannelWidth);
 		}
-		for(int i = 0 ; i < mPoints.size() ; i++) {
-			mColors.add(Color.BLUE);
-		}
-		
-		
 		
 		double midLength = mPoints.get(0).getPointA().getX() + ((numChannel*(mChannelWidth + mSpacing) - mSpacing) / 2);
 		double midHeight = mPoints.get(0).getPointA().midpoint(mPoints.get(mPoints.size() - 1).getPointB()).getY();
 		Point2D midPt = new Point2D(midLength, midHeight);
+		
+		Point2DUtil.subtractPoints(mPoints, midPt);
+		
+		for(int i = 0 ; i < numChannel ; i++) {
+			Point2D port = new Point2D(mPoints.get(i).getPointA().getX() + mChannelWidth / 2, mPoints.get(i).getPointA().getY());
+			addPort(i + 1, port);
+		}
+		Point2D finalPort = new Point2D((mPoints.get(mPoints.size() - 1).getPointB()).getX() - mChannelWidth / 2, (mPoints.get(mPoints.size() - 1).getPointB()).getY());
+		addPort(0, finalPort);
+		
+		for(int i = 0 ; i < mPoints.size() ; i++) {
+			mColors.add(Color.BLUE);
+		}
 	}
 	
 	private List<Point2DPair> getSingleChannel(Point2D startPt) {
