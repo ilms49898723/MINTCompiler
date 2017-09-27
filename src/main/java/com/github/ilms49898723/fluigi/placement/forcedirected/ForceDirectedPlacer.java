bdd3471d433edd6950448b272058c9cd4151d6ca
edinhon <m23127654@gmail.com>
2017-09-27 21:19:24 +0800

    fixed F-D

--- src/main/java/com/github/ilms49898723/fluigi/placement/forcedirected/ForceDirectedPlacer.java
+++ src/main/java/com/github/ilms49898723/fluigi/placement/forcedirected/ForceDirectedPlacer.java
@@ -3,6 +3,7 @@ package com.github.ilms49898723.fluigi.placement.forcedirected;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.ilms49898723.fluigi.device.component.BaseComponent;
 import com.github.ilms49898723.fluigi.device.graph.DeviceGraph;
 import com.github.ilms49898723.fluigi.device.symbol.SymbolTable;
 import com.github.ilms49898723.fluigi.placement.BasePlacer;
@@ -12,14 +13,14 @@ import javafx.geometry.Point2D;
 
 public class ForceDirectedPlacer extends BasePlacer{
 	
-	private List<Integer> mCheckList;
+	private List<Integer> mLockedList;
 
 	public ForceDirectedPlacer(SymbolTable symbolTable, DeviceGraph deviceGraph, Parameters parameters) {
 		super(symbolTable, deviceGraph, parameters);
 		// TODO Auto-generated constructor stub
-		mCheckList = new ArrayList<>();
-		for (int i = 0 ; i < mSymbolTable.size() ; i++) {
-			mCheckList.add(0);
+		mLockedList = new ArrayList<>();
+		for (int i = 0 ; i < mSymbolTable.getComponents().size() ; i++) {
+			mLockedList.add(0);
 		}
 	}
 
@@ -30,9 +31,8 @@ public class ForceDirectedPlacer extends BasePlacer{
 	}
 	
 	private boolean forceDirected() {
-		
-		for (int i = 0 ; i < mSymbolTable.size() ; i++) {
-			if(mCheckList.get(i) == 1) continue;
+		for (int i = 0 ; i < mSymbolTable.getComponents().size() ; i++) {
+			if(mLockedList.get(i) == 1) continue;
 			fixSingleComponentPosition(i);
 		}
 		
@@ -41,18 +41,28 @@ public class ForceDirectedPlacer extends BasePlacer{
 	}
 	
 	private boolean fixSingleComponentPosition (int index) {
-		if(mCheckList.get(index) == 1) return false;
+		if(mLockedList.get(index) == 1) return false;
 		
 		Point2D newPosition = new Point2D(0, 0);//The new position
+		for(int i = 0 ; i < mSymbolTable.getComponents().get(index).getPorts().size() ; i++){
+			if(!mSymbolTable.getComponents().get(index).hasPort(i)) continue;
+			String id = mSymbolTable.getComponents().get(index).getIdentifier();
+			
+			
+		}
+		//Set<DeviceEdge> connectedEdges = mDeviceGraph.edgesOf(source);
+		List<BaseComponent> connectedComponents = new ArrayList<>();
 		//calculate force and find zero-force location
-		mCheckList.set(index, 1);
+		mLockedList.set(index, 1);
 		/*****
 		if (new position is valid) {
 			return true;
 		} else {
 			get the component caused new position invalid
-			if(!fixSingleComponentPosition(invalid idx)) {
+			if(invalid component locked) {
 				fixNearestValidPosition(index);
+			} else {
+				fixSingleComponentPosition(invalid id)
 			}
 		}
 		*****/
@@ -61,7 +71,7 @@ public class ForceDirectedPlacer extends BasePlacer{
 	
 	private boolean fixNearestValidPosition (int index) {
 		//find the smallest force and valid position
-		mCheckList.set(index, 1);
+		mLockedList.set(index, 1);
 		return true;
 	}
 
